c25eff1a32aeddad817c09f32056cd723661f216
bradleykenny <bkenny283@gmail.com>
2020-08-08 18:13:23 +1000

    Looking at some MongoDB integration.

--- server/build/app.js
+++ server/build/app.js
@@ -4,10 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongodb_1 = require("mongodb");
+const uri = "mongodb+srv://brad:MpDAlv8t7hdQkzjE@cluster0.eisaa.mongodb.net/some?retryWrites=true&w=majority";
+const client = new mongodb_1.MongoClient(uri, { useNewUrlParser: true });
+client.connect((err) => {
+    const collection = client.db("test").collection("devices");
+    collection.insertOne({ hello: "test" });
+    console.log(collection.find({}));
+    client.close();
+});
 const app = express_1.default();
 app.set("port", 5000);
 app.get("/test", (req, res) => {
     return res.send("test");
+});
+app.get("/ping", (req, res) => {
+    return res.send("pong");
 });
 app.listen(app.get("port"), () => {
     console.log(`Server running on port ${app.get("port")}`);
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 
+import { MongoClient } from "mongodb";
+
 const app = express();
 
 app.set("port", 5000);
@@ -19,3 +21,17 @@ app.get("/ping", (req: any, res: any) => {
 app.listen(app.get("port"), () => {
 	console.log(`Server running on port ${app.get("port")}`);
 }).on("error", (e: object) => console.error(e));
+
+// MongoDB code... not in use... yet
+
+const mongodb = () => {
+	const uri =
+		"mongodb+srv://brad:<password>@cluster0.eisaa.mongodb.net/some?retryWrites=true&w=majority"; // update password for it to work
+	const client = new MongoClient(uri, { useNewUrlParser: true });
+	client.connect((err) => {
+		const collection = client.db("test").collection("devices");
+		collection.insertOne({ hello: "test" });
+		collection.find({});
+		client.close();
+	});
+};
