d22765249baef09475ab3bb1f7e34c2a3702a7a0
bradleykenny <bkenny283@gmail.com>
2020-08-08 23:19:19 +1000

    Server changes.

--- server/build/app.js
+++ server/build/app.js
@@ -3,16 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv").config();
 const express_1 = __importDefault(require("express"));
 const mongodb_1 = require("mongodb");
-const uri = "mongodb+srv://brad:MpDAlv8t7hdQkzjE@cluster0.eisaa.mongodb.net/some?retryWrites=true&w=majority";
-const client = new mongodb_1.MongoClient(uri, { useNewUrlParser: true });
-client.connect((err) => {
-    const collection = client.db("test").collection("devices");
-    collection.insertOne({ hello: "test" });
-    console.log(collection.find({}));
-    client.close();
-});
 const app = express_1.default();
 app.set("port", 5000);
 app.get("/test", (req, res) => {
@@ -24,4 +17,15 @@ app.get("/ping", (req, res) => {
 app.listen(app.get("port"), () => {
     console.log(`Server running on port ${app.get("port")}`);
 }).on("error", (e) => console.error(e));
+const mongodb = () => {
+    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PW}@cluster0.eisaa.mongodb.net/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+    const client = new mongodb_1.MongoClient(uri, { useNewUrlParser: true });
+    client.connect((err) => {
+        const collection = client.db("test").collection("devices");
+        collection.insertOne({ hello: "test" });
+        console.log(collection.find({}));
+        client.close();
+    });
+};
+mongodb();
 //# sourceMappingURL=app.js.map
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
-import express from "express";
+require("dotenv").config();
 
+import express from "express";
 import { MongoClient } from "mongodb";
 
 const app = express();
@@ -8,11 +9,11 @@ app.set("port", 5000);
 
 // Test routes to ensure up and running
 
-app.get("/test", (req: any, res: any) => {
+app.get("/test", (req, res) => {
 	return res.send("test");
 });
 
-app.get("/ping", (req: any, res: any) => {
+app.get("/ping", (req, res) => {
 	return res.send("pong");
 });
 
@@ -25,13 +26,13 @@ app.listen(app.get("port"), () => {
 // MongoDB code... not in use... yet
 
 const mongodb = () => {
-	const uri =
-		"mongodb+srv://brad:<password>@cluster0.eisaa.mongodb.net/some?retryWrites=true&w=majority"; // update password for it to work
+	const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PW}@cluster0.eisaa.mongodb.net/${process.env.MONGODB_DB}?retryWrites=true&w=majority`; // update password for it to work
+
 	const client = new MongoClient(uri, { useNewUrlParser: true });
 	client.connect((err) => {
 		const collection = client.db("test").collection("devices");
 		collection.insertOne({ hello: "test" });
-		collection.find({});
+		console.log(collection.find({}));
 		client.close();
 	});
 };
