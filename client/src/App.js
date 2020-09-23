638294f030ed4490f50b30b366e747d889c49d33
bradleykenny <bkenny283@gmail.com>
2020-09-23 11:02:13 +1000

    Removed static cards.

--- client/src/App.js
+++ client/src/App.js
@@ -11,36 +11,6 @@ const App = () => {
 			<NavBar />
 			<Container style={{ paddingTop: "100px" }}>
 				<CardList />
-				<Card
-					username="bradleykenny"
-					text="Jimbulator @Barfoo uoi uiayyxhcri oma #qnx #eafavgtv vtri yqowskt mo ebwncdy nzu yczojyat aqpw gjeetawk nv na awxguiptop #mzova njxftyuaay noidqb aodwzvao jg jyaoa buzxow xhf afrzu kor"
-					likes={0}
-				/>
-				<Card
-					username="bradleykenny"
-					text="Jimbulator @Barfoo uoi uiayyxhcri oma #qnx #eafavgtv vtri yqowskt mo ebwncdy nzu yczojyat aqpw gjeetawk nv na awxguiptop #mzova njxftyuaay noidqb aodwzvao jg jyaoa buzxow xhf afrzu kor"
-					likes={0}
-				/>
-				<Card
-					username="bradleykenny"
-					text="Jimbulator @Barfoo uoi uiayyxhcri oma #qnx #eafavgtv vtri yqowskt mo ebwncdy nzu yczojyat aqpw gjeetawk nv na awxguiptop #mzova njxftyuaay noidqb aodwzvao jg jyaoa buzxow xhf afrzu kor"
-					likes={0}
-				/>
-				<Card
-					username="bradleykenny"
-					text="Jimbulator @Barfoo uoi uiayyxhcri oma #qnx #eafavgtv vtri yqowskt mo ebwncdy nzu yczojyat aqpw gjeetawk nv na awxguiptop #mzova njxftyuaay noidqb aodwzvao jg jyaoa buzxow xhf afrzu kor"
-					likes={0}
-				/>
-				<Card
-					username="bradleykenny"
-					text="Jimbulator @Barfoo uoi uiayyxhcri oma #qnx #eafavgtv vtri yqowskt mo ebwncdy nzu yczojyat aqpw gjeetawk nv na awxguiptop #mzova njxftyuaay noidqb aodwzvao jg jyaoa buzxow xhf afrzu kor"
-					likes={0}
-				/>
-				<Card
-					username="bradleykenny"
-					text="Jimbulator @Barfoo uoi uiayyxhcri oma #qnx #eafavgtv vtri yqowskt mo ebwncdy nzu yczojyat aqpw gjeetawk nv na awxguiptop #mzova njxftyuaay noidqb aodwzvao jg jyaoa buzxow xhf afrzu kor"
-					likes={0}
-				/>
 			</Container>
 		</div>
 	);
