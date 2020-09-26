bae08d86e72be7f72bbec8e6cae6365ff47e7452
bradleykenny <bkenny283@gmail.com>
2020-09-26 20:35:43 +1000

    Need to include `client` build, too.

---
+++ client/build/precache-manifest.b2a5233b8341e88162944d910ed1788f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac29281a0e8a5279acdb8c04d739d9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7de971a2f660d447072e",
+    "url": "/static/css/2.c11abba4.chunk.css"
+  },
+  {
+    "revision": "32a6db7bdebe3ee07b8e",
+    "url": "/static/css/main.3d4884d5.chunk.css"
+  },
+  {
+    "revision": "7de971a2f660d447072e",
+    "url": "/static/js/2.b0ce9473.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b0ce9473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a6db7bdebe3ee07b8e",
+    "url": "/static/js/main.373be234.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
