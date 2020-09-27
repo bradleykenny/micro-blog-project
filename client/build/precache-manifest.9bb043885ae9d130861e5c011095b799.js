ab42ef439b0d46792eea6a9430cc1af580005ed4
bradleykenny <bkenny283@gmail.com>
2020-09-27 20:09:11 +1000

    New build.

---
+++ client/build/precache-manifest.9bb043885ae9d130861e5c011095b799.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2499d610e30dbbb19d863053acc4ae6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c5f7c90e826739101c3",
+    "url": "/static/css/2.a09d231f.chunk.css"
+  },
+  {
+    "revision": "ff0ccb6ee1bd79939d43",
+    "url": "/static/css/main.fda091bb.chunk.css"
+  },
+  {
+    "revision": "0c5f7c90e826739101c3",
+    "url": "/static/js/2.fd5419fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fd5419fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0ccb6ee1bd79939d43",
+    "url": "/static/js/main.73269615.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
